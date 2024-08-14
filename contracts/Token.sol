b245ca7598fe3ca3fa0fdeae7b31da4d276d027e
Agentx04 <saintseiya66607@gmail.com>
2024-08-14 05:20:57 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("door","dor"){} 
+    constructor()ERC20("ddoot","der"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
